1bbc97be2684fba0a51181d5a9adbc7321cdf19f
Данил Буляков <bulyakov.hse@gmail.com>
2020-02-27 15:50:54 +0500

    Added IsPalindrome string extension and unit tests for it

---
+++ Palindrome_Server.Tests/Extensions/StringExtensionsIsPalindromeTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome_Server.Extensions;
+
+namespace Palindrome_Server.Tests.Extensions
+{
+    [TestClass]
+    public class StringExtensionsIsPalindromeTests
+    {
+        [TestMethod]
+        public void OneCharString()
+        {
+            var value = "a";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TwoCharString_True()
+        {
+            var value = "aa";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TwoCharString_False()
+        {
+            var value = "ab";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ThreeCharString_True()
+        {
+            var value = "aba";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ThreeCharString_False()
+        {
+            var value = "aab";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void AFetString_True()
+        {
+            var value = "А роза упала на лапу Азора";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsTrue(result);
+        }
+
+            [TestMethod]
+        public void MadamString_True()
+        {
+            var value = "Madam, I’m Adam";
+
+            var result = value.IsPalindrome();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Palindrome_Server/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Palindrome_Server.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string value)
+        {
+            var replaced = Regex.Replace(value.ToLower(), "[^a-zA-Z0-9а-яА-Я]", "");
+            var reversed = new string(replaced.Reverse().ToArray());
+            return Equals(replaced, reversed);
+        }
+    }
+}
